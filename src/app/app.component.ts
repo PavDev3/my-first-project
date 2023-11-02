4a56b9667eb7f8f0c4537d01ee2940bada0ca88f
Pablo <pnunfe@gmail.com>
2023-11-02 13:40:03 +0100

    1st Hello World in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [],
+  template: `<h1>Mi Primera app</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-project';
+  title = 'Primera App';
 }
