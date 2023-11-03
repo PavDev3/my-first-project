9cfcb6dc0de99530619c3aac1ca06b970e766acd
Pablo <pnunfe@gmail.com>
2023-11-03 11:48:34 +0100

    added routes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import  routeConfing  from './app/routes';
 
 bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
+    {providers: 
+      [provideProtractorTestingSupport(), 
+      provideRouter(routeConfing)]})
   .catch(err => console.error(err));
 
